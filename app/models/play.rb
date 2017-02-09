d4477a25d7b9ef3db845aabf9f8d7c8da6f7881e
chechaluque <cesarluque2000@gmail.com>
2017-02-09 15:04:14 -0800

    Review migration between Play, User and Review

--- app/models/play.rb
+++ app/models/play.rb
@@ -1,6 +1,8 @@
 class Play < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  has_many :reviews
+
   validates :title, :description, :director, presence: true
   validates :title, uniqueness: true
 
